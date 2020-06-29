da12b050fe6a9e8ab2b375a972cfabc84479e698
Nicolas <nicolas.boudier@edu.devinci.fr>
2020-06-29 19:23:32 +0200

    third-course

---
+++ js/fonctions.js
@@ -0,0 +1,71 @@
+/**
+ *la fonction prend en paramètre la variable i
+ * on reprend la boucle précédente
+ * on remplace le console.log par return pour ce boolean
+ *
+ */
+nbr = 91;
+
+function nombrePremier(nbr) {
+    for (n = 2; n < o; n++){
+        if (o % n === 0){
+           return false;
+        }
+    }return true;
+}
+
+/**
+ * les fonctions peuvent prendre plusieurs param
+ * ici, si on ne passe pas 2 param, il ne pourra pas associer la valeur aux 2 var
+ */
+function multiples(i, j) {
+    return i * j;
+}
+
+/**
+ *on crée une valeur par défaut pour la variable b afin déviter le return NaN
+ */
+function multiples(i, j) {
+    if (b === undefined){
+        b = 1;
+    }
+    return i * j;
+}
+
+
+/**
+ *Avec un ou plusieurs tableaux
+ */
+tableau = [{
+    nom : 'val1',
+    moy : 15,
+}, {
+    nom : 'val2',
+    moy : 7,
+}, {
+    nom : 'val3',
+    moy : 4,
+}];
+
+
+function moyenne(tableau) {
+    for (i = 0; i < tableau.length; i++){//tant que i < à la taille du tableau, i++
+        tab = tableau[i];//le tableau prend la valeur de i
+        if (tab.moy >= 10){
+            return tableau.nom//si la moyenne >= à 10 alors afficher la var nom
+        }
+    }
+}
+
+
+/**
+ *Une var peut stocker plusieurs méthodes et appeler différentes fonctions
+ */
+var test = {
+    crier : function () {
+        console.log("CRIER");
+    },
+    manger : function () {
+        console.log("MANGER");
+    }
+};
